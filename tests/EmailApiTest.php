f4ff2066b38a8c953758d778505ccf307fbc1d59
Marat Gumerov <marat.gumerof@aspose.com>
2020-03-10 20:49:48 +0400

    testEmailClientMultiAccount added

--- tests/EmailApiTest.php
+++ tests/EmailApiTest.php
@@ -13,7 +13,9 @@ use Aspose\Email\Model\ContactDto;
 use Aspose\Email\Model\EmailAccountConfig;
 use Aspose\Email\Model\EmailAddress;
 use Aspose\Email\Model\EmailClientAccount;
+use Aspose\Email\Model\EmailClientAccountOauthCredentials;
 use Aspose\Email\Model\EmailClientAccountPasswordCredentials;
+use Aspose\Email\Model\EmailClientMultiAccount;
 use Aspose\Email\Model\EmailDto;
 use Aspose\Email\Model\EnumWithCustomOfEmailAddressCategory;
 use Aspose\Email\Model\EnumWithCustomOfPhoneNumberCategory;
@@ -42,15 +44,18 @@ use Aspose\Email\Model\Requests\downloadFileRequest;
 use Aspose\Email\Model\Requests\getCalendarRequest;
 use Aspose\Email\Model\Requests\getContactPropertiesRequest;
 use Aspose\Email\Model\Requests\getEmailClientAccountRequest;
+use Aspose\Email\Model\Requests\getEmailClientMultiAccountRequest;
 use Aspose\Email\Model\Requests\isEmailAddressDisposableRequest;
 use Aspose\Email\Model\Requests\objectExistsRequest;
 use Aspose\Email\Model\Requests\saveCalendarModelRequest;
 use Aspose\Email\Model\Requests\saveContactModelRequest;
 use Aspose\Email\Model\Requests\saveEmailClientAccountRequest;
+use Aspose\Email\Model\Requests\saveEmailClientMultiAccountRequest;
 use Aspose\Email\Model\Requests\saveEmailModelRequest;
 use Aspose\Email\Model\Requests\uploadFileRequest;
 use Aspose\Email\Model\StorageFileLocation;
 use Aspose\Email\Model\StorageFileRqOfEmailClientAccount;
+use Aspose\Email\Model\StorageFileRqOfEmailClientMultiAccount;
 use Aspose\Email\Model\StorageFolderLocation;
 use Aspose\Email\Model\StorageModelRqOfCalendarDto;
 use Aspose\Email\Model\StorageModelRqOfContactDto;
@@ -435,6 +440,42 @@ class EmailApiTest extends TestCase
             ((object)$account->getCredentials())->getPassword(),
             ((object)$result->getCredentials())->getPassword());
         $this->assertEquals($account->getHost(), $result->getHost());
+    }
+
+    /**
+     * @group pipeline
+     */
+    public function testEmailClientMultiAccount(): void
+    {
+        // Create multi account object
+        $multiAccount = new EmailClientMultiAccount(
+            array(
+                new EmailClientAccount('imap.gmail.com', 993, 'SSLAuto', 'IMAP',
+                    new EmailClientAccountPasswordCredentials(
+                        'example@gmail.com', null, 'password')),
+                    new EmailClientAccount('exchange.outlook.com', 443, 'SSLAuto', 'EWS',
+                        new EmailClientAccountOauthCredentials(
+                            'example@outlook.com', null, 'clientId', 'clientSecret', 'refreshToken'))),
+            new EmailClientAccount('smtp.gmail.com', 465, 'SSLAuto', 'SMTP',
+                new EmailClientAccountPasswordCredentials(
+                    'example@gmail.com', null, 'password')));
+        $api = self::getApi();
+        $folder = self::$folder;
+        $storage = self::$storage;
+        $fileName = uniqid() . ".multi.account";
+        // Save multi account
+        $api->saveEmailClientMultiAccount(new SaveEmailClientMultiAccountRequest(
+            new StorageFileRqOfEmailClientMultiAccount(
+                $multiAccount,
+                new StorageFileLocation($storage, $folder, $fileName))));
+        // Get multi account object from storage
+        $multiAccountFromStorage = $api->getEmailClientMultiAccount(
+            new GetEmailClientMultiAccountRequest($fileName, $folder, $storage));
+
+        $this->assertEquals(2, count($multiAccountFromStorage->getReceiveAccounts()));
+        $this->assertEquals(
+            $multiAccount->getSendAccount()->getCredentials()->getDiscriminator(),
+            $multiAccountFromStorage->getSendAccount()->getCredentials()->getDiscriminator());
     }
 
     private function createCalendar(DateTime $startDate = null) : string
